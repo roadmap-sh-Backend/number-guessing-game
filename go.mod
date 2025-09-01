da7a3956e992fe61e4579b0c9eedf62fd8741be5
Akihira <tik.dikawahyu123@gmail.com>
2025-09-01 08:26:22 +0700

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/roadmap-sh-Backend/number-guessing-game
+
+go 1.24.3
---
+++ main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"math/rand"
+	"os"
+)
+
+func main() {
+	fmt.Println("Welcome to the Number Guessing Game!")
+	fmt.Println("I'm thinking of a number between 1 and 100.")
+	fmt.Println("You have some chances based on the difficult to guess the correct number.")
+
+	fmt.Println("\nPlease select the difficulty level:")
+	fmt.Println("1. Easy (10 chances)")
+	fmt.Println("2. Medium (5 chances)")
+	fmt.Println("3. Hard (3 chances)")
+
+	fmt.Print("\nEnter your choice: ")
+	var choice int16
+	maxAttempt := 0
+	fmt.Scan(&choice)
+
+	switch choice {
+	case 1:
+		fmt.Println("Great! You have selected the Easy difficulty level.")
+		maxAttempt = 10
+	case 2:
+		fmt.Println("Great! You have selected the Medium difficulty level.")
+		maxAttempt = 5
+	case 3:
+		fmt.Println("Great! You have selected the Hard difficulty level.")
+		maxAttempt = 3
+	default:
+		log.Fatal("Unknown difficulty. The difficult range are 1 to 3 (easy to hard)")
+	}
+	fmt.Println("Let's start the game!")
+
+	target := rand.Intn(101)
+	if target == 0 {
+		target = 100
+	}
+
+	var input int
+	attempt := 0
+	for attempt < maxAttempt {
+		fmt.Print("\nEnter your guess: ")
+		fmt.Scan(&input)
+
+		if input > target {
+			fmt.Printf("Incorrect! The number is less than %d.\n", input)
+		} else if input < target {
+			fmt.Printf("Incorrect! The number is greater than %d.\n", input)
+		} else {
+			fmt.Printf("Congratulations! You guessed the correct number in %d attempts.\n", attempt)
+			os.Exit(0)
+		}
+
+		attempt++
+	}
+
+	fmt.Printf("Game Over! You exceeeded max attempts. The correct number is %d\n", target)
+}
